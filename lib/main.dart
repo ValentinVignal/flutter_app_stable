edb94246a142bef3082e78dcd95c2ad0947e8ccc
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-08-11 10:04:16 +0800

    :alembic: Add drawer page

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      redirect: (_) => '/persons',
+    ),
+    GoRoute(
+      path: '/persons',
+      builder: (_, __) => const PersonsScreen(),
+      routes: [
+        GoRoute(
+          path: ':id',
+          pageBuilder: (_, __) => const DrawerPage(
+            child: PersonScreen(),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class PersonsScreen extends StatelessWidget {
+  const PersonsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.grey,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Persons'),
+            ElevatedButton(
+              onPressed: () => GoRouter.of(context).push('/persons/1'),
+              child: const Text('Push'),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class PersonScreen extends StatelessWidget {
+  const PersonScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Person'),
+            ElevatedButton(
+              onPressed: GoRouter.of(context).pop,
+              child: const Text('Pop'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerPage extends Page {
+  const DrawerPage({
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Route createRoute(BuildContext context) {
+    return _DrawerPageRoute(
+      settings: this,
+    );
+  }
+}
+
+class _DrawerPageRoute extends TransitionRoute {
+  _DrawerPageRoute({
+    required DrawerPage settings,
+  }) : super(
+          settings: settings,
+        );
+  @override
+  Iterable<OverlayEntry> createOverlayEntries() {
+    return [
+      OverlayEntry(
+        builder: (context) {
+          return Row(
+            children: [
+              const Spacer(flex: 2),
+              Expanded(
+                  child: SlideTransition(
+                position: animation!.drive(
+                  Tween(
+                    begin: const Offset(1, 0),
+                    end: const Offset(0, 0),
+                  ),
+                ),
+                child: (settings as DrawerPage).child,
+              )),
+            ],
+          );
+        },
+      ),
+    ];
+  }
+
+  @override
+  bool get opaque => false;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 200);
 }
