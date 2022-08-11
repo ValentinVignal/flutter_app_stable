dcb8f5a7ccd0f082b7de295ead7138f405dcb691
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-08-11 18:18:47 +0800

    :alembic: don't animate top bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,176 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'data.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  App({Key? key}) : super(key: key);
+
+  static const title = 'GoRouter Example: Nested Navigation';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp.router(
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
+        routeInformationProvider: _router.routeInformationProvider,
+        title: title,
+      );
+
+  late final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        redirect: (_) => '/family/${Families.data[0].id}',
+      ),
+      GoRoute(
+        path: '/family/:fid',
+        builder: (context, state) => FamilyTabsScreen(
+          // key: state.pageKey,
+          selectedFamily: Families.family(state.params['fid']!),
+        ),
+        routes: [
+          GoRoute(
+            path: 'person/:pid',
+            builder: (context, state) {
+              final family = Families.family(state.params['fid']!);
+              final person = family.person(state.params['pid']!);
+
+              return PersonScreen(family: family, person: person);
+            },
+          ),
+        ],
+      ),
+    ],
+
+    // show the current router location as the user navigates page to page; note
+    // that this is not required for nested navigation but it is useful to show
+    // the location as it changes
+    // navigatorBuilder: (context, state, child) => Material(
+    //   child: Column(
+    //     children: [
+    //       Expanded(child: child),
+    //       Padding(
+    //         padding: const EdgeInsets.all(8),
+    //         child: Text(state.location),
+    //       ),
+    //     ],
+    //   ),
+    // ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FamilyTabsScreen extends StatefulWidget {
+  FamilyTabsScreen({required Family selectedFamily, Key? key})
+      : index = Families.data.indexWhere((f) => f.id == selectedFamily.id),
+        super(key: key) {
+    assert(index != -1);
+  }
 
-  // This widget is the root of your application.
+  final int index;
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  _FamilyTabsScreenState createState() => _FamilyTabsScreenState();
+}
+
+class _FamilyTabsScreenState extends State<FamilyTabsScreen>
+    with TickerProviderStateMixin {
+  late final TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(
+      length: Families.data.length,
+      vsync: this,
+      initialIndex: widget.index,
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  void didUpdateWidget(FamilyTabsScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.index = widget.index;
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Column(
+          children: [
+            const Text(App.title),
+            TabBar(
+              controller: _controller,
+              tabs: [for (final f in Families.data) Tab(text: f.name)],
+              onTap: (index) => _tap(context, index),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: TabBarView(
+                controller: _controller,
+                children: [
+                  for (final f in Families.data) FamilyView(family: f)
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+
+  void _tap(BuildContext context, int index) =>
+      context.go('/family/${Families.data[index].id}');
+}
+
+class FamilyView extends StatefulWidget {
+  const FamilyView({required this.family, Key? key}) : super(key: key);
+  final Family family;
+
+  @override
+  State<FamilyView> createState() => _FamilyViewState();
+}
+
+/// Use the [AutomaticKeepAliveClientMixin] to keep the state, like scroll
+/// position and text fields when switching tabs, as well as when popping back
+/// from sub screens. To use the mixin override [wantKeepAlive] and call
+/// `super.build(context)` in build.
+///
+/// In this example if you make a web build and make the browser window so low
+/// that you have to scroll to see the last person on each family tab, you will
+/// see that state is kept when you switch tabs and when you open a person
+/// screen and pop back to the family.
+class _FamilyViewState extends State<FamilyView> {
+  // Override `wantKeepAlive` when using `AutomaticKeepAliveClientMixin`.
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (final p in widget.family.people)
+          ListTile(
+            title: Text(p.name),
+            onTap: () =>
+                context.go('/family/${widget.family.id}/person/${p.id}'),
+          ),
+      ],
     );
   }
+}
+
+class PersonScreen extends StatelessWidget {
+  const PersonScreen({required this.family, required this.person, Key? key})
+      : super(key: key);
+
+  final Family family;
+  final Person person;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: Text(person.name)),
+        body: Text('${person.name} ${family.name} is ${person.age} years old'),
+      );
 }
