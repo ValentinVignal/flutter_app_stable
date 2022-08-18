fc93bf059a7e4032aa8c0ccba33c8b82685d059e
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-08-18 15:51:34 +0800

    :alembic: Minimal code to reproduce the issue

--- lib/main.dart
+++ lib/main.dart
@@ -4,112 +4,121 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+final useRouteInformationProvider = ValueNotifier(false);
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  final routeDelegate = MyRouterDelegate();
+  final routeInformationParser = MyRouteInformationParser();
+  final routeInformationProvider = PlatformRouteInformationProvider(
+    initialRouteInformation: const RouteInformation(
+      location: '/',
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return AnimatedBuilder(
+      animation: useRouteInformationProvider,
+      builder: (context, _) {
+        return MaterialApp.router(
+          // key: ValueKey(useRouteInformationProvider.value),
+          routerDelegate: routeDelegate,
+          routeInformationParser: routeInformationParser,
+          routeInformationProvider: useRouteInformationProvider.value
+              ? routeInformationProvider
+              : null,
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+enum Pages {
+  page1,
+  page2;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Page get page => MaterialPage(child: MyWidget(page: this));
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyRouterDelegate extends RouterDelegate<Pages>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  var currentPage = Pages.page1;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        currentPage.page,
+      ],
+      onPopPage: (route, result) {
+        return true;
+      },
+    );
+  }
+
+  @override
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Future<void> setNewRoutePath(Pages configuration) async {
+    currentPage = configuration;
+  }
+}
+
+class MyRouteInformationParser extends RouteInformationParser<Pages> {
+  @override
+  Future<Pages> parseRouteInformation(RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location!);
+    if (uri.pathSegments.isNotEmpty && uri.pathSegments.first == 'page2') {
+      return Pages.page2;
+    }
+    return Pages.page1;
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    required this.page,
+    Key? key,
+  }) : super(key: key);
+
+  final Pages page;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Column(
+        children: <Widget>[
+          Text('Page: $page'),
+          ElevatedButton(
+            child: const Text('Go to other'),
+            onPressed: () {},
+          ),
+          ElevatedButton(
+            child: const Text('Use route information provider'),
+            onPressed: () {
+              useRouteInformationProvider.value =
+                  !useRouteInformationProvider.value;
+            },
+          ),
+          AnimatedBuilder(
+            animation: useRouteInformationProvider,
+            builder: (context, _) {
+              return Text(
+                  'useRouteInformationProvider: ${useRouteInformationProvider.value}');
+            },
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
