8f5160a8b38f42fbe7af9d3bb6f15539cb17e201
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-09-23 16:47:18 +0800

    :alembic: Test with GoRouter.of(context) and AnimatedBuilder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,24 +6,85 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app_stable/main.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
+
+class GoRouterBackButton extends StatelessWidget {
+  const GoRouterBackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final router = GoRouter.of(context);
+    print(
+      'GoRouterBackButton.build - canPop ${router.canPop()}',
+    );
+    return AnimatedBuilder(
+      // This forces the widget to rebuild when a page is pushed/popped. This is
+      // useful when the page pushed is transparent.
+      animation: router,
+      builder: (context, child) {
+        print(
+          'GoRouterBackButton.build.AnimatedBuilder.builder - canPop ${router.canPop()}',
+        );
+        return Visibility(
+          visible: router.canPop(),
+          child: child!,
+        );
+      },
+      child: BackButton(
+        onPressed: router.pop,
+      ),
+    );
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets(
+      'It should display a back button when there is a page to pop and pop it when pressed',
+      (tester) async {
+    final router = GoRouter(
+      initialLocation: '/a',
+      routes: [
+        GoRoute(
+          path: '/a',
+          builder: (_, __) => const Material(child: GoRouterBackButton()),
+        ),
+        GoRoute(
+          path: '/b',
+          builder: (_, __) => const Material(child: GoRouterBackButton()),
+        ),
+      ],
+    );
+    final widget = MaterialApp.router(
+      routerConfig: router,
+    );
+    await tester.pumpWidget(widget);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(
+      find.byType(BackButton),
+      findsNothing,
+      reason: 'When there is nothing to pop, it should not display anything',
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    print('Before push');
+    router.push('/b');
+    await tester.pumpAndSettle(); // Navigate between the pages.
+    expect(router.canPop(), true);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(
+      find.byType(BackButton),
+      findsOneWidget,
+      reason: 'It should display a back button when there is a page to pop',
+    );
+
+    print('Before pop');
+    await tester.tap(find.byType(BackButton));
+    await tester.pumpAndSettle(); // Navigate between the pages.
+
+    expect(
+      find.byType(BackButton),
+      findsNothing,
+      reason: 'When there is nothing to pop, it should not display anything',
+    );
   });
 }
