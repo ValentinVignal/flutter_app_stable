f337b8163ba3cbe7122f87a406c41633eb8a7642
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-10-19 16:51:49 +0800

    :alembic: Failing test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,43 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app_stable/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+final streamProvider = StreamProvider<int>((ref) async* {
+  final completer = Completer<void>();
+  final timer = Timer(const Duration(seconds: 1), () {
+    completer.complete();
+  });
+  ref.onDispose(() {
+    if (timer.isActive) {
+      timer.cancel();
+    }
+    if (!completer.isCompleted) {
+      completer.complete();
+    }
+  });
+  await completer.future; // <- Remove this line to make the test passes.
+  throw Error();
+});
+
+class MyWidget extends ConsumerWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(streamProvider);
+    return const SizedBox();
+  }
+}
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Test stream', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MyWidget(),
+      ),
+    );
+    await tester.pump(); // <- Add Duration(seconds: 1) and it doesn't fail.
   });
 }
