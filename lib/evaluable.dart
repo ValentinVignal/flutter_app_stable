2f79d14f457c9e1598140d4629b683ce868a02e7
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-10-20 23:22:10 +0800

    :fire: Remove libraries

--- lib/evaluable.dart
+++ lib/evaluable.dart
@@ -1,32 +1,19 @@
-import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
-
-part 'evaluable.g.dart';
 part 'evaluable_text.dart';
 
 abstract class Evaluable<TReturnedType> {
   Evaluable();
-
-  factory Evaluable.fromJson(Map<String, dynamic> json) {
-    return FieldValueEvaluable<TReturnedType>.fromJson(json);
-  }
-
-  Map<String, dynamic> toJson();
 
   /// Evaluates the [Evaluable] with the given [input].
   TReturnedType evaluate();
 }
 
 abstract class SingleEntryEvaluable<TReturnedType, TValueType>
-    extends Evaluable<TReturnedType> with EquatableMixin {
+    extends Evaluable<TReturnedType> {
   SingleEntryEvaluable({
     required this.value,
   });
 
   final TValueType value;
-
-  @override
-  List<Object?> get props => [value];
 }
 
 abstract class FieldValueEvaluable<TReturnedType>
@@ -34,9 +21,4 @@ abstract class FieldValueEvaluable<TReturnedType>
   FieldValueEvaluable({
     required super.value,
   });
-
-  factory FieldValueEvaluable.fromJson(Map<String, dynamic> json) {
-    return TextFieldValueEvaluable.fromJson(json)
-        as FieldValueEvaluable<TReturnedType>;
-  }
 }
--- lib/evaluable_text.dart
+++ lib/evaluable_text.dart
@@ -3,22 +3,11 @@ part of 'evaluable.dart';
 /// {@template nl.evaluable.text_field_value}
 /// A evaluable that evaluates to the text of a field in the [EvaluableInput].
 /// {@endtemplate}
-@JsonSerializable()
 class TextFieldValueEvaluable extends FieldValueEvaluable<String> {
   /// {@macro nl.evaluable.text_field_value}
   TextFieldValueEvaluable({
     required super.value,
   }) : super();
-
-  /// {@macro nl.evaluable.text_field_value}
-  factory TextFieldValueEvaluable.fromJson(Map<String, dynamic> json) {
-    return _$TextFieldValueEvaluableFromJson(json);
-  }
-
-  @override
-  Map<String, dynamic> toJson() {
-    return _$TextFieldValueEvaluableToJson(this);
-  }
 
   @override
   String evaluate() {
