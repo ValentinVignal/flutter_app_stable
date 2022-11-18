5f36c605ab5372f953129994007641f9a00c8eee
ValentinVignal <valentin.vignal.dev@outlook.fr>
2022-11-18 18:15:18 +0800

    :alembic: Issue with dialog not being rebuilt

--- lib/main.dart
+++ lib/main.dart
@@ -4,112 +4,222 @@ void main() {
   runApp(const MyApp());
 }
 
+class AppRoute {
+  const AppRoute(this.location);
+  final int? location;
+}
+
+class AppRouter extends RouterDelegate<AppRoute>
+    with PopNavigatorRouterDelegateMixin, ChangeNotifier {
+  // get correct state of router
+  @override
+  AppRoute get currentConfiguration => AppRoute(_selected);
+
+  int? _selected;
+  int get selected => _selected!;
+  set selected(int value) {
+    _selected = value == 0 ? null : value;
+    notifyListeners();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: _navigatorKey,
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+        _selected = null;
+        notifyListeners();
+        return true;
+      },
+      pages: [
+        const MaterialPage(
+          key: ValueKey('home'),
+          child: Home(),
+        ),
+        if (_selected != null && _selected! > 0)
+          DialogPage(
+            key: ValueKey('dialog-$_selected'),
+            child: DialogWidget(
+              parameter: _selected!,
+            ),
+          )
+        else if (_selected != null && _selected! < 0)
+          MaterialPage(
+            key: const ValueKey('material'),
+            child: DialogWidget(
+              parameter: _selected!,
+            ),
+          )
+      ],
+    );
+  }
+
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  @override
+  GlobalKey<NavigatorState> get navigatorKey => _navigatorKey;
+
+  @override
+  Future<void> setNewRoutePath(AppRoute configuration) async {
+    _selected = configuration.location;
+  }
+}
+
+class AppRouteInformationParser extends RouteInformationParser<AppRoute> {
+  // This converts route state to route information.
+  @override
+  RouteInformation restoreRouteInformation(configuration) {
+    final String location;
+    if (configuration.location == null) {
+      location = '/';
+    } else {
+      location = '/dialog/${configuration.location}';
+    }
+    return RouteInformation(location: location);
+  }
+
+// This converts route info to router state
+  @override
+  Future<AppRoute> parseRouteInformation(
+      RouteInformation routeInformation) async {
+    final int? selected;
+    if (routeInformation.location?.startsWith('/dialog/') ?? false) {
+      selected = int.parse(routeInformation.location!.split('/').last);
+    } else {
+      selected = null;
+    }
+    return AppRoute(selected);
+  }
+}
+
+final delegate = AppRouter();
+final informationParser = AppRouteInformationParser();
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routerDelegate: delegate,
+      routeInformationParser: informationParser,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                delegate.selected = 1;
+              },
+              child: const Text('Dialog'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                delegate.selected = -1;
+              },
+              child: const Text('Material'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DialogWidget extends StatefulWidget {
+  const DialogWidget({
+    required this.parameter,
+    super.key,
+  });
+
+  final int parameter;
+
+  @override
+  State<DialogWidget> createState() => _DialogWidgetState();
+}
+
+class _DialogWidgetState extends State<DialogWidget> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState ${widget.parameter}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: const Text('Dialog'),
+      children: [
+        Row(
+          children: [
+            IconButton(
+              onPressed: () {
+                delegate.selected--;
+              },
+              icon: const Icon(Icons.arrow_back_ios),
+            ),
+            Text(widget.parameter.toString()),
+            IconButton(
+              onPressed: () {
+                delegate.selected++;
+              },
+              icon: const Icon(Icons.arrow_forward_ios),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DialogPage extends Page {
+  const DialogPage({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Route createRoute(BuildContext context) {
+    return _DialogRoute(
+      settings: this,
+      context: context,
+      builder: (context) {
+        ModalRoute.of(context);
+        return child;
+      },
+    );
+  }
+}
+
+class _DialogRoute extends DialogRoute {
+  _DialogRoute({
+    super.settings,
+    required super.context,
+    required super.builder,
+  });
+
+  @override
+  bool canTransitionFrom(TransitionRoute previousRoute) {
+    return previousRoute is! _DialogRoute;
+  }
+
+  @override
+  bool canTransitionTo(TransitionRoute nextRoute) {
+    return nextRoute is! _DialogRoute;
   }
 }
