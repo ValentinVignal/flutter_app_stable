8d2bb3ef54d0a599491dd958202c4ca06fa56a9a
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-02-06 16:17:59 +0800

    :alembic: Use requestController.add

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,81 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:ferry/ferry.dart';
+import 'package:ferry_flutter/ferry_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_stable/graphql/__generated__/pokemons_query.data.gql.dart';
+import 'package:flutter_app_stable/graphql/__generated__/pokemons_query.req.gql.dart';
+import 'package:flutter_app_stable/graphql/__generated__/pokemons_query.var.gql.dart';
+import "package:gql_exec/gql_exec.dart" hide Operation;
 
 void main() {
   runApp(const MyApp());
 }
 
+var count = 0;
+
+Iterable<GPokemonsData_pokemons> generatePokemon() sync* {
+  for (var i = 0; i < count; i++) {
+    yield GPokemonsData_pokemons((pokemon) => pokemon
+      ..id = 'pokemonId-$i'
+      ..name = 'PokemonName$i');
+  }
+}
+
+final link = Link.function((request, [forward]) {
+  count++;
+  print('request $count');
+  return Stream.value(Response(
+    response: const {},
+    data: GPokemonsData(
+      (data) => data
+        ..pokemons = ListBuilder<GPokemonsData_pokemons>(
+          generatePokemon(),
+        ),
+    ).toJson(),
+  ));
+});
+
+final client = Client(
+  link: link,
+  defaultFetchPolicies: const {
+    OperationType.query: FetchPolicy.NetworkOnly,
+  },
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        body: Operation<GPokemonsData, GPokemonsVars>(
+          // Give an id to the request here to make it work.
+          operationRequest: GPokemonsReq(),
+          client: client,
+          builder: (context, response, error) {
+            return ListView.builder(
+              itemCount: response?.data?.pokemons.length ?? 0,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(response!.data!.pokemons[index].name),
+                );
+              },
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Does nothing. Add a request ID here and to the
+            // `Operation.operationRequest` and it ref-etches the list of
+            // pokemons.
+            client.requestController.add(
+              GPokemonsReq((req) => req.requestId = 'pokemons'),
+            );
+          },
+          child: const Icon(Icons.add),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
