bbbd349b573122be7765ca31487e4558ca8a9d3c
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-04-06 09:53:52 +0800

    :alembic: Override providers raise an issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final a = Provider.autoDispose<String>(
+  (ref) {
+    return 'a0';
+  },
+  name: 'a',
+);
+
+final b = Provider.autoDispose<String>(
+  (ref) {
+    return 'b0';
+  },
+  name: 'b',
+);
+
+final c = Provider.autoDispose<String>(
+  (ref) {
+    return ref.watch(a) + ref.watch(b);
+  },
+  name: 'c',
+  dependencies: [a, b],
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: ProviderScope(
+        overrides: [
+          a.overrideWithValue('a1'),
+        ],
+        child: ProviderScope(
+          overrides: [
+            a.overrideWithValue('a2'),
+          ],
+          child: const MyWidget(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyWidget extends ConsumerStatefulWidget {
+  const MyWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ConsumerState<MyWidget> createState() => _MyWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyWidgetState extends ConsumerState<MyWidget> {
+  var _show = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(ref.watch(a)),
+            // If you open, close and open again, you will see the error:
+            //
+            // The following assertion was thrown building Consumer(dirty, dependencies:
+            // [UncontrolledProviderScope], state: _ConsumerState#652b3):
+            // Tried to read c0:AutoDisposeProvider<String>#a27a4 from a place where one of its dependencies were
+            // overridden but the provider is not.
+            //
+            // To fix this error, you can add "dependencies" to c0:AutoDisposeProvider<String>#a27a4 such that we
+            // have:
+            //
+            // ```
+            // final a = Provider(...);
+            // final b = Provider((ref) => ref.watch(a), dependencies: [a]);
+            // ```
+            // 'package:riverpod/src/framework/container.dart':
+            // Failed assertion: line 443 pos 17: 'targetElement.provider != targetElement.origin ||
+            //                     dependencyElement ==
+            //                         targetElement.container
+            //                             .readProviderElement<Object?>(dependency)'
+            //
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _show = !_show;
+                });
+              },
+              child: const Text('Toggle'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            if (_show) ...[
+              ProviderScope(
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    return Text(ref.watch(c));
+                  },
+                ),
+              ),
+            ]
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
