887a2167a259994322822a43459f2d8a44e7a87e
ValentinVignal <valentin.vignal.dev@outlook.fr>
2023-05-26 14:37:11 +0800

    :fire: Fragment with no graphql

--- lib/__generated__/serializers.gql.dart
+++ lib/__generated__/serializers.gql.dart
@@ -11,30 +11,6 @@ import 'package:flutter_app_stable/graphql/__generated__/fragment.pokemon.req.gq
     show GPokemonFragmentReq;
 import 'package:flutter_app_stable/graphql/__generated__/fragment.pokemon.var.gql.dart'
     show GPokemonFragmentVars;
-import 'package:flutter_app_stable/graphql/__generated__/query.pokemons_0.data.gql.dart'
-    show GPokemons0Data, GPokemons0Data_pokemons;
-import 'package:flutter_app_stable/graphql/__generated__/query.pokemons_0.req.gql.dart'
-    show GPokemons0Req;
-import 'package:flutter_app_stable/graphql/__generated__/query.pokemons_0.var.gql.dart'
-    show GPokemons0Vars;
-import 'package:flutter_app_stable/graphql/__generated__/query.pokemons_1.data.gql.dart'
-    show GPokemons1Data, GPokemons1Data_pokemons;
-import 'package:flutter_app_stable/graphql/__generated__/query.pokemons_1.req.gql.dart'
-    show GPokemons1Req;
-import 'package:flutter_app_stable/graphql/__generated__/query.pokemons_1.var.gql.dart'
-    show GPokemons1Vars;
-import 'package:flutter_app_stable/graphql/__generated__/query.polemon_0.data.gql.dart'
-    show GPokemon0Data, GPokemon0Data_pokemon;
-import 'package:flutter_app_stable/graphql/__generated__/query.polemon_0.req.gql.dart'
-    show GPokemon0Req;
-import 'package:flutter_app_stable/graphql/__generated__/query.polemon_0.var.gql.dart'
-    show GPokemon0Vars;
-import 'package:flutter_app_stable/graphql/__generated__/query.polemon_1.data.gql.dart'
-    show GPokemon1Data, GPokemon1Data_pokemon;
-import 'package:flutter_app_stable/graphql/__generated__/query.polemon_1.req.gql.dart'
-    show GPokemon1Req;
-import 'package:flutter_app_stable/graphql/__generated__/query.polemon_1.var.gql.dart'
-    show GPokemon1Vars;
 import 'package:gql_code_builder/src/serializers/operation_serializer.dart'
     show OperationSerializer;
 
@@ -44,24 +20,8 @@ final SerializersBuilder _serializersBuilder = _$serializers.toBuilder()
   ..add(OperationSerializer())
   ..addPlugin(StandardJsonPlugin());
 @SerializersFor([
-  GPokemon0Data,
-  GPokemon0Data_pokemon,
-  GPokemon0Req,
-  GPokemon0Vars,
-  GPokemon1Data,
-  GPokemon1Data_pokemon,
-  GPokemon1Req,
-  GPokemon1Vars,
   GPokemonFragmentData,
   GPokemonFragmentReq,
   GPokemonFragmentVars,
-  GPokemons0Data,
-  GPokemons0Data_pokemons,
-  GPokemons0Req,
-  GPokemons0Vars,
-  GPokemons1Data,
-  GPokemons1Data_pokemons,
-  GPokemons1Req,
-  GPokemons1Vars,
 ])
 final Serializers serializers = _serializersBuilder.build();
--- lib/__generated__/serializers.gql.g.dart
+++ lib/__generated__/serializers.gql.g.dart
@@ -7,34 +7,9 @@ part of 'serializers.gql.dart';
 // **************************************************************************
 
 Serializers _$serializers = (new Serializers().toBuilder()
-      ..add(FetchPolicy.serializer)
-      ..add(GPokemon0Data.serializer)
-      ..add(GPokemon0Data_pokemon.serializer)
-      ..add(GPokemon0Req.serializer)
-      ..add(GPokemon0Vars.serializer)
-      ..add(GPokemon1Data.serializer)
-      ..add(GPokemon1Data_pokemon.serializer)
-      ..add(GPokemon1Req.serializer)
-      ..add(GPokemon1Vars.serializer)
       ..add(GPokemonFragmentData.serializer)
       ..add(GPokemonFragmentReq.serializer)
-      ..add(GPokemonFragmentVars.serializer)
-      ..add(GPokemons0Data.serializer)
-      ..add(GPokemons0Data_pokemons.serializer)
-      ..add(GPokemons0Req.serializer)
-      ..add(GPokemons0Vars.serializer)
-      ..add(GPokemons1Data.serializer)
-      ..add(GPokemons1Data_pokemons.serializer)
-      ..add(GPokemons1Req.serializer)
-      ..add(GPokemons1Vars.serializer)
-      ..addBuilderFactory(
-          const FullType(
-              BuiltList, const [const FullType(GPokemons0Data_pokemons)]),
-          () => new ListBuilder<GPokemons0Data_pokemons>())
-      ..addBuilderFactory(
-          const FullType(
-              BuiltList, const [const FullType(GPokemons1Data_pokemons)]),
-          () => new ListBuilder<GPokemons1Data_pokemons>()))
+      ..add(GPokemonFragmentVars.serializer))
     .build();
 
 // ignore_for_file: deprecated_member_use_from_same_package,type=lint
