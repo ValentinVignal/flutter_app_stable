9354fd90636b0c0b76ee1d1608d400aad4ece3bf
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-04-19 17:56:53 +0800

    :alembic: Reproduce with riverpod 2.5.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(
@@ -10,177 +9,80 @@ void main() {
   );
 }
 
-/// If `true`, use `GoRouter.replace`, if `false`, use
-/// `GoRouter.pushReplacement`.
-final useReplaceProvider = StateProvider.autoDispose<bool>((ref) {
-  return true;
-});
-
-final intUrlParameterProvider = Provider.autoDispose<int?>((ref) {
-  return null;
-});
-
-/// `templateProvider`
-final intDatabaseProvider = StreamProvider.autoDispose.family<int, int>(
-  (ref, id) async* {
-    await Future.delayed(const Duration(seconds: 1));
-    yield 42;
-  },
-);
-
-/// `templateEffectiveVersionNumberProvider`
-final sumUrlParameterAndDatabaseProvider =
-    Provider.autoDispose.family<int, int>(
-  (ref, id) {
-    final url = ref.watch(intUrlParameterProvider);
-    final database = ref.watch(intDatabaseProvider(id)).valueOrNull ?? 0;
-    return (url ?? 0) + database;
-  },
-  dependencies: [intUrlParameterProvider, intDatabaseProvider],
-);
-
-final router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) {
-        final count = int.tryParse(state.uri.queryParameters['count'] ?? '');
-        final id = int.tryParse(state.uri.queryParameters['id'] ?? '') ?? 0;
-        return ProviderScope(
-          overrides: [
-            intUrlParameterProvider.overrideWithValue(count),
-          ],
-          child: Home(
-            id: id,
-          ),
-        );
-      },
-    ),
-  ],
-);
-
-void redirect(BuildContext context, WidgetRef ref) {
-  final location = '/?count=${(ref.read(intUrlParameterProvider) ?? 0) + 1}';
-  final useReplace = ref.read(useReplaceProvider);
-  if (useReplace) {
-    GoRouter.of(context).replace(location);
-  } else {
-    GoRouter.of(context).pushReplacement(location);
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-    );
-  }
-}
-
-class AppBarTitle extends StatelessWidget {
-  const AppBarTitle({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(GoRouterState.of(context).uri.toString());
-  }
-}
-
-class Home extends ConsumerWidget {
-  const Home({
+class MyApp extends ConsumerWidget {
+  const MyApp({
     super.key,
-    required this.id,
   });
 
-  final int id;
-
-  @override
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const AppBarTitle(),
-        actions: [
-          Switch(
-            value: ref.watch(useReplaceProvider),
-            onChanged: (value) {
-              ref.read(useReplaceProvider.notifier).state = value;
-            },
-          ),
+    return MaterialApp(
+      home: ProviderScope(
+        overrides: [
+          parameterProvider.overrideWithValue(null),
         ],
-      ),
-      body: Content(id: id),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          redirect(context, ref);
-        },
-        child: const Icon(Icons.add),
+        child: const Scaffold(
+          body: Column(
+            children: [
+              Widget1(),
+              Widget2(),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-class Content extends StatelessWidget {
-  const Content({
-    super.key,
-    required this.id,
-  });
-
-  final int id;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Content1(id: id),
-        ),
-        Expanded(
-          child: Content2(id: id),
-        ),
-      ],
-    );
-  }
-}
-
-class Content1 extends ConsumerWidget {
-  const Content1({
-    super.key,
-    required this.id,
-  });
-
-  final int id;
+class Widget1 extends ConsumerWidget {
+  const Widget1({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final sum = ref.watch(sumUrlParameterAndDatabaseProvider(id));
-    return Center(
-      child: Text(
-        'Content1: $sum',
-      ),
-    );
+    final version = ref.watch(combinedProvider) != null;
+
+    return Text(version.toString());
   }
 }
 
-class Content2 extends ConsumerWidget {
-  const Content2({
-    super.key,
-    required this.id,
-  });
-
-  final int id;
+class Widget2 extends ConsumerWidget {
+  const Widget2({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final sum = ref.watch(sumUrlParameterAndDatabaseProvider(id));
-    return Center(
-      child: Text(
-        'Content2: $sum',
-      ),
-    );
+    final isPublished = ref.watch(combinedProvider) != null;
+    final versions = ref.watch(providerNotifier);
+    return Text('versions ${versions != null}, $isPublished');
   }
 }
+
+final parameterProvider = Provider.autoDispose<int?>((ref) => null);
+
+final streamProvider = StreamProvider.autoDispose<int>((ref) async* {
+  yield 42;
+});
+
+final providerNotifier =
+    StateNotifierProvider.autoDispose<Notifier, int?>((ref) {
+  return Notifier(ref.watch(streamProvider).valueOrNull);
+}, dependencies: [streamProvider]);
+
+class Notifier extends StateNotifier<int?> {
+  Notifier(super.state);
+}
+
+final effectiveParameterProvider = Provider.autoDispose<int?>(
+  (ref) {
+    return ref.watch(parameterProvider) ?? ref.watch(providerNotifier);
+  },
+  dependencies: [parameterProvider, providerNotifier],
+);
+
+final combinedProvider = Provider.autoDispose<int?>(
+  (ref) {
+    final param = ref.watch(effectiveParameterProvider) ?? 0;
+    final value = ref.watch(providerNotifier) ?? 0;
+    return value + param;
+  },
+  dependencies: [effectiveParameterProvider, providerNotifier],
+);
