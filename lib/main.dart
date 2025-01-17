55ca11b0f0ed77164a824913c053a80250bcf71b
ValentinVignalPru <valentin.vignal@prudential.com.sg>
2025-01-17 18:13:52 +0800

    Filtered query example with riverpod and go router

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,169 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+part 'main.g.dart';
+
+@TypedGoRoute<HomeRoute>(path: '/')
+class HomeRoute extends GoRouteData {
+  const HomeRoute();
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const HomePage();
+  }
+}
+
+@TypedGoRoute<ILPRoute>(path: '/ilp')
+class ILPRoute extends GoRouteData {
+  const ILPRoute({
+    this.type,
+  });
+
+  final int? type;
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return ProviderScope(
+      overrides: [
+        filterProvider.overrideWith((ref) => type),
+      ],
+      // ignore: prefer_const_constructors
+      child: ILPPage(),
+    );
+  }
+}
+
+class SetWrapper<T> with EquatableMixin {
+  const SetWrapper([this.value = const {}]);
+
+  final Set<T> value;
+
+  @override
+  List<Object?> get props => [value];
+}
+
+final router = GoRouter(
+  routes: $appRoutes,
+);
 
 void main() {
-  runApp(const MyApp());
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              const ILPRoute().go(context);
+            },
+            child: const Text('ILP'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              const ILPRoute(type: 2).go(context);
+            },
+            child: const Text('ILP filtered'),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+class Transaction {
+  const Transaction({
+    required this.id,
+    required this.type,
+  });
+
+  final String id;
+  final int type;
+}
+
+final transactionsProvider = FutureProvider.autoDispose((ref) async {
+  await Future.delayed(const Duration(seconds: 1));
+
+  return const [
+    Transaction(id: '1', type: 1),
+    Transaction(id: '2', type: 2),
+    Transaction(id: '3', type: 3),
+    Transaction(id: '4', type: 4),
+  ];
+});
+
+final filterProvider = StateProvider.autoDispose<int?>((ref) {
+  throw UnimplementedError('This should be overridden');
+});
+
+final filteredTransactionProvider =
+    Provider.autoDispose<AsyncValue<List<Transaction>>>(
+  (ref) {
+    final transactions = ref.watch(transactionsProvider);
+    final filters = ref.watch(filterProvider);
+
+    if (filters == null) return transactions;
+    return transactions.whenData((transactions) {
+      return transactions
+          .where((transaction) => filters == transaction.type)
+          .toList();
+    });
+  },
+  dependencies: [transactionsProvider, filterProvider],
+);
+
+class ILPPage extends ConsumerWidget {
+  const ILPPage({super.key, int? type});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final transactions = ref.watch(filteredTransactionProvider);
+    final filters = ref.watch(filterProvider);
+    return Scaffold(
+      body: Column(
+        children: [
+          ToggleButtons(
+            isSelected: [1, 2, 3, 4].map((t) => filters == t).toList(),
+            children: [1, 2, 3, 4].map((t) => Text(t.toString())).toList(),
+            onPressed: (index) {
+              if (filters == index + 1) {
+                ref.read(filterProvider.notifier).state = null;
+                return;
+              }
+              ref.read(filterProvider.notifier).state = index + 1;
+            },
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                for (final transaction in transactions.valueOrNull ?? const [])
+                  ListTile(
+                    title: Text(transaction.id),
+                    subtitle: Text(transaction.type.toString()),
+                  ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/main.g.dart
@@ -0,0 +1,70 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<RouteBase> get $appRoutes => [
+      $homeRoute,
+      $iLPRoute,
+    ];
+
+RouteBase get $homeRoute => GoRouteData.$route(
+      path: '/',
+      factory: $HomeRouteExtension._fromState,
+    );
+
+extension $HomeRouteExtension on HomeRoute {
+  static HomeRoute _fromState(GoRouterState state) => const HomeRoute();
+
+  String get location => GoRouteData.$location(
+        '/',
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+RouteBase get $iLPRoute => GoRouteData.$route(
+      path: '/ilp',
+      factory: $ILPRouteExtension._fromState,
+    );
+
+extension $ILPRouteExtension on ILPRoute {
+  static ILPRoute _fromState(GoRouterState state) => ILPRoute(
+        type: _$convertMapValue('type', state.uri.queryParameters, int.parse),
+      );
+
+  String get location => GoRouteData.$location(
+        '/ilp',
+        queryParams: {
+          if (type != null) 'type': type!.toString(),
+        },
+      );
+
+  void go(BuildContext context) => context.go(location);
+
+  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location);
+
+  void replace(BuildContext context) => context.replace(location);
+}
+
+T? _$convertMapValue<T>(
+  String key,
+  Map<String, String> map,
+  T Function(String) converter,
+) {
+  final value = map[key];
+  return value == null ? null : converter(value);
+}
