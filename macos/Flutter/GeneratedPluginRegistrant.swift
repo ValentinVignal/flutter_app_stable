ee561d5c4633a71c42907713e849ce6bfbf83b57
ValentinVignalPru <valentin.vignal@prudential.com.sg>
2025-01-31 13:55:19 +0800

    Run android

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
-import firebase_messaging
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
 }
