10f6675f39e476104d090458325bec89a8e42c6e
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-10-15 09:58:17 +0800

    google font issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:widgetbook/widgetbook.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Widgetbook.material(
-      initialRoute: '?path=use_case',
-      directories: [
-        WidgetbookUseCase(
-          name: 'use_case',
+    return MaterialApp(
+      home: Material(
+        child: Builder(
           builder: (context) {
             final theme = Theme.of(context);
             final withGoogleFont = theme.copyWith(
@@ -26,50 +22,38 @@ class MyApp extends StatelessWidget {
                 ),
               ),
             );
-            return Theme(data: withGoogleFont, child: UseCase());
-          },
-        ),
-        WidgetbookUseCase(
-          name: 'other',
-          builder: (context) {
-            return SizedBox();
-          },
-        ),
-      ],
-    );
-  }
-}
-
-class UseCase extends StatelessWidget {
-  const UseCase({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return ColoredBox(
-      color: theme.scaffoldBackgroundColor,
-      child: Center(
-        child: IntrinsicWidth(
-          child: Column(
-            spacing: 8,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.min,
-                spacing: 8,
-                children: [
-                  Flexible(
-                    child: Text(
-                      'Option 1',
-                      style: theme.textTheme.displayLarge,
+            return Theme(
+              data: withGoogleFont,
+              child: Builder(
+                builder: (context) {
+                  return Center(
+                    child: IntrinsicWidth(
+                      child: Column(
+                        spacing: 8,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.min,
+                            spacing: 8,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  'Option 1',
+                                  style: theme.textTheme.displayLarge,
+                                ),
+                              ),
+                              Icon(Icons.keyboard_arrow_down_rounded),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Icon(Icons.keyboard_arrow_down_rounded),
-                ],
+                  );
+                },
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
