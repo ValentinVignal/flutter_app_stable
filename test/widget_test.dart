d256df90bf69045689e8fb7ef3522f6cfe3688a9
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-10-23 11:03:36 +0800

    intl Customize month

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,27 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_app_stable/main.dart';
+import 'package:intl/date_symbol_data_custom.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/date_symbols.dart';
+import 'package:intl/date_time_patterns.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('Counter increments smoke test', () async {
+    await initializeDateFormatting('en_US', null);
+    final initialDateTimeSymbols = dateTimeSymbolMap();
+    final en = initialDateTimeSymbols['en_US']!;
+    final serialized = en.serializeToMap();
+    serialized['STANDALONESHORTMONTHS'] = [
+      ...serialized['STANDALONESHORTMONTHS'],
+    ]..[8] = 'Sept';
+    final modifiedEn = DateSymbols.deserializeFromMap(serialized);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    initializeDateFormattingCustom(
+      locale: 'en_US',
+      patterns: dateTimePatternMap()['en_US'],
+      symbols: modifiedEn,
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    print(DateFormat.MMM('en_US').format(DateTime(2025, 9, 22)));
   });
 }
